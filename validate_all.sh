be8ddd784ed18bd1876b4c6c811c3bd8f5e9c340
FabijanC <fabijan.corak@gmail.com>
2023-12-06 16:36:06 +0100

    Improve validate_all.sh

--- validate_all.sh
+++ validate_all.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
-set -u
+set -eu
 
 # by relying on a counter, we ensure that all files are validated and exit code is non-zero if any fail
 failure_count=0
 
 for spec_file in "api/starknet_api_openrpc.json" "api/starknet_trace_api_openrpc.json" "api/starknet_write_api.json"; do
-    node validate.js $spec_file || failure_count=$((failure_count + 1))
+    node validate.js "$spec_file" || failure_count=$((failure_count + 1))
 done
 
 exit $failure_count
