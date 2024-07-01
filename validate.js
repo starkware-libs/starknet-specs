b69b972bf9ce9e78ef1b6cf064c236e15e25045c
amanusk <amanusk@pm.me>
2024-07-01 12:46:42 +0300

    Add .editorconfig and format files

--- validate.js
+++ validate.js
@@ -2,27 +2,26 @@
 
 const { parseOpenRPCDocument, validateOpenRPCDocument, dereferenceDocument } = require("@open-rpc/schema-utils-js");
 const Dereferencer = require("@json-schema-tools/dereferencer");
-const fs = require('fs-extra');
+const fs = require("fs-extra");
 
 async function runValidation(filename) {
-    try {
-        let docToParse = await fileContentAsJSON(filename)
-        let docToParseWithExternalRefs = await fetchExternalRefsFor(docToParse);
-        let dereffedDoc = await derefAll(docToParseWithExternalRefs);
+  try {
+    let docToParse = await fileContentAsJSON(filename);
+    let docToParseWithExternalRefs = await fetchExternalRefsFor(docToParse);
+    let dereffedDoc = await derefAll(docToParseWithExternalRefs);
 
-        let doc = await parseOpenRPCDocument(dereffedDoc, { dereference: true });
+    let doc = await parseOpenRPCDocument(dereffedDoc, { dereference: true });
 
-        const errors = validateOpenRPCDocument(doc);
-        if (errors === true) {
-            console.log("Ok!")
-        } else {
-            console.error(errors.name)
-            console.error(errors.message)
-        }
+    const errors = validateOpenRPCDocument(doc);
+    if (errors === true) {
+      console.log("Ok!");
+    } else {
+      console.error(errors.name);
+      console.error(errors.message);
     }
-    catch (exn) {
-        console.error(exn && exn.message)
-    }
+  } catch (exn) {
+    console.error(exn && exn.message);
+  }
 }
 
 /**
@@ -32,11 +31,11 @@ async function runValidation(filename) {
  * @returns The amended objects.
  */
 async function fetchExternalRefsFor(docToParse) {
-    let externalRefs = await externalRefsFrom(docToParse.components.schemas);
-    let allExternalSchemas = await fetchExternalSchemas(externalRefs);
-    Object.keys(allExternalSchemas).forEach(key => docToParse.components.schemas[key] = allExternalSchemas[key]);
+  let externalRefs = await externalRefsFrom(docToParse.components.schemas);
+  let allExternalSchemas = await fetchExternalSchemas(externalRefs);
+  Object.keys(allExternalSchemas).forEach(key => (docToParse.components.schemas[key] = allExternalSchemas[key]));
 
-    return docToParse;
+  return docToParse;
 }
 
 /**
@@ -47,8 +46,8 @@ async function fetchExternalRefsFor(docToParse) {
  * @returns The amended dereferencer.
  */
 function fixRefs(dereffer) {
-    dereffer.refs = dereffer.refs.filter(r => dereffer.refCache[r] === undefined);
-    return dereffer;
+  dereffer.refs = dereffer.refs.filter(r => dereffer.refCache[r] === undefined);
+  return dereffer;
 }
 
 /**
@@ -58,25 +57,25 @@ function fixRefs(dereffer) {
  * @returns The document, with the references resolved.
  */
 async function derefAll(doc) {
-
-    let allSchemas = doc.components.schemas;
-    let refCacheWithRecursiveRef = {
-        "#/components/schemas/NESTED_CALL": allSchemas["NESTED_CALL"]
-    }
-    let dereferencerOptions = {
-        refCache: refCacheWithRecursiveRef,
-        rootSchema: doc
-    }
-    //resolve all schemas, and remember them
-    await Promise.all(Object.keys(allSchemas).map(async k => {
-        let s = allSchemas[k]
-        let dereffer = fixRefs(new Dereferencer.default(s, dereferencerOptions));
-        allSchemas[k] = await dereffer.resolve();
-        return allSchemas[k];
-    }))
-    return doc;
+  let allSchemas = doc.components.schemas;
+  let refCacheWithRecursiveRef = {
+    "#/components/schemas/NESTED_CALL": allSchemas["NESTED_CALL"],
+  };
+  let dereferencerOptions = {
+    refCache: refCacheWithRecursiveRef,
+    rootSchema: doc,
+  };
+  //resolve all schemas, and remember them
+  await Promise.all(
+    Object.keys(allSchemas).map(async k => {
+      let s = allSchemas[k];
+      let dereffer = fixRefs(new Dereferencer.default(s, dereferencerOptions));
+      allSchemas[k] = await dereffer.resolve();
+      return allSchemas[k];
+    }),
+  );
+  return doc;
 }
-
 
 /**
  * Retrieve external schema definitions, from other files.
@@ -84,26 +83,29 @@ async function derefAll(doc) {
  * @returns The actual referenced schema objects
  */
 async function fetchExternalSchemas(externalRefs) {
-    let externalFilenames = Object.values(externalRefs).map(ref => filenameFromExternalRef(ref.value))
-    let uniqueExtFilenames = [...new Set(externalFilenames)]
-    let externalJSONPromises = uniqueExtFilenames
-        .map(relative => { return { key: relative, fullpath: fullPathForRefFile(relative) } })
-        .map(entry => fileContentAsJSON(entry.fullpath).then(c => {
-            return { key: entry.key, content: c }
-        }));
+  let externalFilenames = Object.values(externalRefs).map(ref => filenameFromExternalRef(ref.value));
+  let uniqueExtFilenames = [...new Set(externalFilenames)];
+  let externalJSONPromises = uniqueExtFilenames
+    .map(relative => {
+      return { key: relative, fullpath: fullPathForRefFile(relative) };
+    })
+    .map(entry =>
+      fileContentAsJSON(entry.fullpath).then(c => {
+        return { key: entry.key, content: c };
+      }),
+    );
 
-    let externalJSONs = await Promise.all(externalJSONPromises);
+  let externalJSONs = await Promise.all(externalJSONPromises);
 
-    let ret = {}
+  let ret = {};
 
-    //collect all schema objects into `ret` and return it.
-    externalJSONs
-        .map(entry => entry.content.components.schemas)
-        //note: this means that if we have duplicates, the last file will win. not a problem for now.
-        .forEach(schemaParent => Object.keys(schemaParent)
-            .forEach(k => ret[k] = schemaParent[k]))
+  //collect all schema objects into `ret` and return it.
+  externalJSONs
+    .map(entry => entry.content.components.schemas)
+    //note: this means that if we have duplicates, the last file will win. not a problem for now.
+    .forEach(schemaParent => Object.keys(schemaParent).forEach(k => (ret[k] = schemaParent[k])));
 
-    return ret;
+  return ret;
 }
 
 /**
@@ -112,8 +114,8 @@ async function fetchExternalSchemas(externalRefs) {
  * @returns The parsed JS object.
  */
 async function fileContentAsJSON(filename) {
-    let json = await fs.readJson(filename)
-    return json
+  let json = await fs.readJson(filename);
+  return json;
 }
 
 /**
@@ -123,13 +125,14 @@ async function fileContentAsJSON(filename) {
  * @returns The external schema references, keyed by their original keys.
  */
 async function externalRefsFrom(allSchemas) {
+  const isExternalRef = ref => ref && ref.length > 0 && ref.charAt(0) != "#";
 
-    const isExternalRef = ref => ref && ref.length > 0 && ref.charAt(0) != "#";
-
-    let externalKeys = Object.keys(allSchemas)
-        .filter(k =>
-            allSchemas[k]["$ref"] !== undefined && isExternalRef(allSchemas[k]["$ref"]))
-    return externalKeys.map(k => { return { key: k, value: allSchemas[k]["$ref"] }; });
+  let externalKeys = Object.keys(allSchemas).filter(
+    k => allSchemas[k]["$ref"] !== undefined && isExternalRef(allSchemas[k]["$ref"]),
+  );
+  return externalKeys.map(k => {
+    return { key: k, value: allSchemas[k]["$ref"] };
+  });
 }
 
 /**
@@ -146,13 +149,10 @@ const filenameFromExternalRef = ref => ref.split("#")[0];
  * @param {String} relative The relative path
  * @returns The full filesystem path
  */
-const fullPathForRefFile = relative => `${process.cwd()}/${relative}` //should canonize this
+const fullPathForRefFile = relative => `${process.cwd()}/${relative}`; //should canonize this
 
 let args = process.argv.slice(2);
 
 if (args.length > 0) {
-    runValidation(args[0])
-}
-else
-    console.error("Missing filename");
-
+  runValidation(args[0]);
+} else console.error("Missing filename");
